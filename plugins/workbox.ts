6719d506dc9b08f43d1bf5f63bfa68f4d20e96e7
Lukas Zbinden <lukas.zbinden@digital4you.ch>
2022-01-16 15:05:03 +0100

    nuxtpwa and personal mix

---
+++ plugins/workbox.ts
@@ -0,0 +1,21 @@
+import { Plugin } from '@nuxt/types'
+import { Workbox } from 'workbox-window'
+
+const workbox: Plugin = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+
+  if ('serviceWorker' in navigator) {
+    const wb = new Workbox('/sw.js', { scope: '/' })
+
+    wb.register().catch((err) => {
+      const logElement = document.getElementById('log')
+      if (logElement) {
+        logElement.innerHTML = err
+      }
+    })
+  }
+}
+
+export default workbox
