7f4665524391bf73ffd1dbfc14622c7a6ae462ae
Lukas Zbinden <lukas.zbinden@digital4you.ch>
2022-01-16 12:34:36 +0100

    added refresh

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -2,13 +2,24 @@
   <div>
     <h1 class="text-2xl">PWA Playground Test Page</h1>
     <nuxt-link to="/">Link to index</nuxt-link>
+    <button @click="refresh">refresh page</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, useRouter } from '@nuxtjs/composition-api'
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const refresh = () => {
+      window.location.reload()
+    }
+
+    return {
+      refresh
+    }
+  },
+})
 </script>
 
 <style></style>
