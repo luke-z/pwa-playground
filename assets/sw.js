102fab4bbc1a3d9435f6ac9940cfcff5c653fd22
Lukas Zbinden <37207714+luke-z@users.noreply.github.com>
2022-01-17 14:37:42 +0100

    test cache index

--- assets/sw.js
+++ assets/sw.js
@@ -21,6 +21,7 @@ setCacheNameDetails({
 const manifest = self.__WB_MANIFEST
 const customRoutes = [
   { url: '/?standalone=true', revision: Date.now().toString() },
+  { url: '/', revision: Date.now().toString() },
   { url: '/test', revision: null },
   { url: '/test/deep', revision: null },
   { url: '/manifest.json', revision: Date.now().toString() },
