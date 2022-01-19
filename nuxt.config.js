c39c328133ae7556a01ad70d509593d3c80cf5e9
Lukas Zbinden <lukas.zbinden@digital4you.ch>
2022-01-19 15:23:58 +0100

    rev6

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,10 +63,10 @@ export default {
         swDest: resolve('./static/sw.js'),
         maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
         additionalManifestEntries: [
-          { url: '/', revision: '5' },
-          { url: '/test', revision: '5' },
+          { url: '/', revision: '6' },
+          { url: '/test', revision: '6' },
           // { url: '/test/', revision: '5' },
-          { url: '/test/deep', revision: '5' },
+          { url: '/test/deep', revision: '6' },
           // { url: '/test/deep/', revision: '5' },
         ],
         directoryIndex: '/',
